cfa60d8b97bc3ce0abde4a0e363f0387c53bacfe
Ash <artdoctorka@gmail.com>
2024-04-13 16:32:05 +0200

    make agent work

--- agent.py
+++ agent.py
@@ -8,16 +8,16 @@ from langchain_openai import ChatOpenAI
 llm = ChatOpenAI(model="gpt-3.5-turbo-0125")
 
 from langchain.agents import AgentExecutor, create_tool_calling_agent
-from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain_core.prompts import ChatPromptTemplate
+from tools import AddClass, EndConversation, ShowClasses
 
-tools = [TavilySearchResults(max_results=1)]
+tools = [AddClass(), EndConversation(), ShowClasses()]
 
 prompt = ChatPromptTemplate.from_messages(
     [
         (
             "system",
-            "You are a helpful assistant. Make sure to use the tavily_search_results_json tool for information.",
+            "You are an assistant in developing bots. Your goal is to talk with the user to understand their needs and then use the tools to define the bot using CMB class. The CMB class is just text explaining one of the bots dialog options (example - 'get yes/no answer', 'ask user about something', 'end conversation').",
         ),
         ("placeholder", "{chat_history}"),
         ("human", "{input}"),
@@ -30,4 +30,8 @@ agent = create_tool_calling_agent(llm, tools, prompt)
 
 # Create an agent executor by passing in the agent and tools
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
-agent_executor.invoke({"input": "what is LangChain?"})
+current_message = ''
+
+while True:
+    current_message = input("Enter your message: ")
+    agent_executor.invoke({"input": current_message})
--- tools.py
+++ tools.py
@@ -1,5 +1,10 @@
 from typing import Optional, Type
 from pydantic import BaseModel
+from langchain.callbacks.manager import (
+    AsyncCallbackManagerForToolRun,
+    CallbackManagerForToolRun,
+)
+from langchain_core.tools import BaseTool
 
 
 cmb_classes = []
@@ -16,7 +21,45 @@ class ShowClasses(BaseTool):
         self, query: str, run_manager: Optional[CallbackManagerForToolRun] = None
     ) -> str:
         """Use the tool."""
-        return "LangChain"
+        return str(cmb_classes)
+
+    async def _arun(
+        self, query: str, run_manager: Optional[AsyncCallbackManagerForToolRun] = None
+    ) -> str:
+        """Use the tool asynchronously."""
+        raise NotImplementedError("custom_search does not support async")
+
+
+class AddClass(BaseTool):
+    name = "add_cmb_class"
+    description = "use this tool to add a new CMB class. Just explain what this class does in text."
+    args_schema: Type[BaseModel] = None
+
+    def _run(
+        self, query: str, run_manager: Optional[CallbackManagerForToolRun] = None
+    ) -> str:
+        """Use the tool."""
+        cmb_classes.append(query)
+        return "Class added successfully"
+
+    async def _arun(
+        self, query: str, run_manager: Optional[AsyncCallbackManagerForToolRun] = None
+    ) -> str:
+        """Use the tool asynchronously."""
+        raise NotImplementedError("custom_search does not support async")
+
+
+class EndConversation(BaseTool):
+    name = "end_conversation"
+    description = "When you have enough data about the bot and CMB classes, use this tool to end conversation."
+    args_schema: Type[BaseModel] = None
+
+    def _run(
+        self, query: str, run_manager: Optional[CallbackManagerForToolRun] = None
+    ) -> str:
+        """Use the tool."""
+        print('#'*10, 'Conversation ended', '#'*10)
+        print('CMB classes:', cmb_classes)
 
     async def _arun(
         self, query: str, run_manager: Optional[AsyncCallbackManagerForToolRun] = None
